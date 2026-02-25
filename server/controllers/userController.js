1e2f14926ed5355c8d661f9c8346edfcb538541f
AyushiJat10 <aayushijat8@gmail.com>
2026-02-25 21:47:55 +0530

    created user data api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -49,4 +49,13 @@ export const loginUser = async (req,res)=>{
     }
 }
 
-
+//get user data using token
+export const getUserData = async(req,res)=>{
+    try {
+        const {user} =req;
+        res.json({success:true,user})
+    } catch (error) {
+        onsole.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
