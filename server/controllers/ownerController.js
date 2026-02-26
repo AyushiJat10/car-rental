8a133ad157b4740b5e46288962b2315317153f0f
AyushiJat10 <aayushijat8@gmail.com>
2026-02-26 10:25:58 +0530

    all the api's of backend are created

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -2,6 +2,7 @@ import User from "../models/user.js";
 import fs from "fs";
 import imagekit from "../configs/imagekit.js";
 import Car from "../models/Car.js";
+import Booking from "../models/Booking.js";
 
 
 //change role to owner from user
@@ -50,4 +51,125 @@ export const addCar = async(req, res)=>{
         console.log(error.message);
         res.json({success: false, message: error.message})
     }
+}
+
+//API to listowner cars
+export const getOwnerCars = async (req,res)=>{
+    try {
+        const {_id} = req.user;
+        const cars = await Car.find({owner:_id})
+        res.json({success:true,cars})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+//api to toggle car availability
+export const toggleAvailability = async(req,res)=>{
+    try {
+        const {_id} = req.user;
+        const {carId} = req.body;
+        const car = await Car.findById(carId)
+        //check if car belong to owner
+        if(car.owner.toString() !== _id.toString()){
+            return res.json({success: false, message: "unauthorized"})
+        }
+        car.isAvailable = !car.isAvailable;
+        await car.save()
+        res.json({success:true, message:"Availability toggled"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+//owner can delete their car api
+export const deleteCar = async(req,res)=>{
+    try {
+        const {_id} = req.user;
+        const {carId} = req.body;
+        const car = await Car.findById(carId)
+        //check if car belong to owner
+        if(car.owner.toString() !== _id.toString()){
+            return res.json({success: false, message: "unauthorized"})
+        }
+        car.owner = null;
+        car.isAvailable = false;
+        await car.save()
+        res.json({success:true, message:"Car removed"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+//api to get dashboard data
+export const getDashboardData = async (req,res)=>{
+try {
+    const {_id , role} = req.user;
+    if(role !== 'owner'){
+        return res.json({success:false, message: "unauthorized"})
+    }
+
+    const cars = await Car.find({owner: _id})
+    const bookings = (await Booking.find({owner: _id}).populate('car')).sort({createdAt: -1});
+    const pendingBookings = await Booking.find({owner : _id, status: "pending"})
+     const completedBookings = await Booking.find({owner : _id, status: "confirmed"})
+
+ //calculated monthly revenue
+    const monthlyRevenue = bookings.slice().filter(booking=>booking.status === "confirmed").reduce((acc,booking)=> acc+booking.price ,0)
+ 
+    const dashboardData = {
+        totalCars : cars.length,
+        totalBookings : bookings.length,
+        pendingBookings: pendingBookings.length,
+        completedBookings: completedBookings.length,
+        recentBookings: bookings.slice(0,3),
+        monthlyRevenue
+    }
+    res.json({success:true , dashboardData});
+
+} catch (error) {
+    console.log(error.message);
+        res.json({success: false, message: error.message})
+}
+}
+
+//api to update user image
+export const updateUserImage = async (req,res)=>{
+    try{
+        const {_id} = req.user;
+        const imageFile = req.file;
+        
+        //upload image to imagekit
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder:'/cars'
+        })
+        
+        //optimize through imagekit url transformation
+        var optimizedImageUrl = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {width: '400'},  //width resizing
+                {quality: 'auto'}, //auto compression
+                {format : 'webp'}  //convert to modern format
+            ]
+
+        })
+
+        const image = optimizedImageUrl;
+
+        await User.findByIdAndUpdate(_id,{image});
+        res.json({success:true , message: "image updated"})
+    }catch (error) {
+    console.log(error.message);
+        res.json({success: false, message: error.message})
+}
 }
