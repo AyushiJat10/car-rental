0d4643ccce256f18a0ddc29010bde86d30ca08e3
Rajmahiii21 <rajm93415@gmail.com>
2026-03-02 22:58:49 +0530

    adding mybooking

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -17,9 +17,7 @@ const MyBookings = () => {
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-32  2xl:px-48 mt-16 text-sm
     max-w-7xl'>
-      <Title title='My Bookings'
-      subTitle='View and manage your all car bookiongs'
-      align="left"/>
+      <Title title='My Bookings' subTitle='View and manage your all car bookings' align="left"/>
 
       <div>
         {bookings.map((bookings, index)=>(
